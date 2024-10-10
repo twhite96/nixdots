5d6736fae0eb93acee9ee73d4d9dd4dcd663c9a3
twhite96 <zimalite@protonmail.ch>
2024-10-10 02:24:18 -0400

    Add config

---
+++ flake.nix
@@ -0,0 +1,31 @@
+# flake.nix
+
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
+  };
+
+  outputs = {nixpkgs, home-manager, hyprland, ...}: {
+    homeConfigurations."user@hostname" = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+
+      modules = [
+        {
+          wayland.windowManager.hyprland = {
+            enable = true;
+            # set the flake package
+            package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+          };
+        }
+        # ...
+      ];
+    };
+  };
+}
